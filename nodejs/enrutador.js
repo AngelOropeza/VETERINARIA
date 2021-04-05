2e6c0484d5236c55360b7eeda4608120cfecefcf
AngelOropeza <qookiescream@gmail.com>
2021-04-05 13:00:37 -0500

    Refactorizar -> modular enrutador

---
+++ nodejs/enrutador.js
@@ -0,0 +1,26 @@
+module.exports = {
+    ruta: (data, callback)=>{ //handler
+        callback(200,{mensaje: 'esta es ruta'});
+    },
+    mascotas: {
+        get: (data, callback)=>{
+            if(typeof data.indice !== 'undefined'){
+                console.log('handler mascotas',{data})
+                if(global.recursos.mascotas[data.indice]){
+                    return callback(200, global.recursos.mascotas[data.indice]);
+                }
+                return callback(404, {
+                    mensaje: `mascota con indice ${data.indice} no encontrado`
+                });
+            }
+            callback(200, global.recursos.mascotas);
+        },
+        post: (data, callback)=>{
+            global.recursos.mascotas.push(data.payload);
+            callback(201, data.payload); //StatusCode 201 => CREATED
+        }
+    },
+    noEncontrado: (data, callback)=>{
+        callback(404,{mensaje: 'no encontrado'});
+    }
+}
