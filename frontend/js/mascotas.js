7ba7ed885e2c13b0874170b85884839740d4722b
AngelOropeza <qookiescream@gmail.com>
2021-04-07 13:08:51 -0500

    Listar mascotas en la aplicacion web desde la API

--- frontend/js/mascotas.js
+++ frontend/js/mascotas.js
@@ -6,42 +6,35 @@ const indice = document.getElementById('indice')
 const form = document.getElementById('form')
 const btnGuardar = document.getElementById('btn-guardar')
 
-let mascotas = [
-    {
-        tipo : "Perro",
-        nombre: "Manchas",
-        dueno: "Esteban"
-    },
-    {
-        tipo : "Gato",
-        nombre: "Menso",
-        dueno: "Julian"
-    },
-    {
-        tipo : "Perro",
-        nombre: "Manchas",
-        dueno: "Esteban"
+let mascotas = [];
+
+async function listarMascotas(){
+    try {
+        const respuesta = await fetch('http://localhost:5000/mascotas');
+        const mascotasDelServer = await respuesta.json();
+        if(Array.isArray(mascotasDelServer) && mascotasDelServer.length > 0){
+            mascotas = mascotasDelServer;
+        }        
+        const htmlMascotas = mascotas
+        .map(
+            (mascota, index)=>`<tr>
+            <th scope="row">${index}</th>
+            <td>${mascota.tipo}</td>
+            <td>${mascota.nombre}</td>
+            <td>${mascota.dueno}</td>
+            <td>
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-info editar" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="far fa-edit"></i></button>
+                <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            </td>
+        </tr>`).join("");
+        listaMascotas.innerHTML = htmlMascotas;
+        Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
+        Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index));
+    } catch (error) {
+        throw error;
     }
-];
-
-function listarMascotas(){
-    solicitarMascota();
-    const htmlMascotas = mascotas.map((mascota, index)=>`<tr>
-        <th scope="row">${index}</th>
-        <td>${mascota.tipo}</td>
-        <td>${mascota.nombre}</td>
-        <td>${mascota.dueno}</td>
-        <td>
-        <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-info editar" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="far fa-edit"></i></button>
-            <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash-alt"></i></button>
-        </div>
-        </td>
-    </tr>`).join("");
-    listaMascotas.innerHTML = htmlMascotas;
-    Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
-    Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index));
-
 }
 
 function enviarDatos(evento){
@@ -94,16 +87,6 @@ function eliminar(index){
 }
 
 listarMascotas();
-
-function solicitarMascota(){
-    fetch('http://localhost:5000/mascotas').then((respuesta)=>{
-        if(respuesta.ok){
-            return respuesta.json();
-        }
-    }).then(mascotasDelServer=>{
-        console.log(mascotasDelServer);
-    });
-}
 
 form.onsubmit = enviarDatos;
 btnGuardar.onclick = enviarDatos;
