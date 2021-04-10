8d5ef3170b274aac90cd0c99e347dc6dc65b5961
AngelOropeza <qookiescream@gmail.com>
2021-04-09 22:38:21 -0500

    Listar veterinarios desde API

--- frontend/js/veterinarias.js
+++ frontend/js/veterinarias.js
@@ -1,67 +1,92 @@
 const pais = document.getElementById('pais');
-const identificacion = document.getElementById('identificacion');
+const documento = document.getElementById('documento');
 const nombre = document.getElementById('nombre');
 const apellido = document.getElementById('apellido');
 const indice = document.getElementById('indice');
 const form = document.getElementById('form');
 const btnGuardar = document.getElementById('btn-guardar');
 const listaVeterinarias = document.getElementById('lista-veterinarias');
+const url = 'http://localhost:5000/veterinarias';
 
-let veterinarias = [
-    {
-        nombre: "Naryie",
-        apellido : "Vazquez",
-        pais: "Colombia",
-        identificacion: "1234567890"
-    },
-    {
-        nombre: "Juan David",
-        apellido : "Marín",
-        pais: "Colombia",
-        identificacion: "1234567899"
-    },
-];
 
-function listarVeterinarias(){
-    const htmlVeterinarias = veterinarias.map((veterinaria, index)=>`<tr>
-        <th scope="row">${index}</th>
-        <td>${veterinaria.identificacion}</td>
-        <td>${veterinaria.pais}</td>
-        <td>${veterinaria.nombre}</td>
-        <td>${veterinaria.apellido}</td>
-        <td>
-        <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-info editar" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="far fa-edit"></i></button>
-            <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash-alt"></i></button>
-        </div>
-        </td>
-    </tr>`).join("");
-    listaVeterinarias.innerHTML = htmlVeterinarias;
-    Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
-    Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index));
-}
+let veterinarias = [];
 
-function enviarDatos(evento){
-    evento.preventDefault();
-    const datos = {
-        nombre: nombre.value,
-        apellido: apellido.value,
-        pais: pais.value,
-        identificacion: identificacion.value
-    };
-    const accion = btnGuardar.innerHTML
-    switch(accion){
-        case 'Editar':
-            //editar
-            veterinarias[indice.value] = datos;
-            break;
-        default:
-            //Crear
-            veterinarias.push(datos);
-            break;
+async function listarVeterinarias() {
+    try {
+      const respuesta = await fetch(url);
+      const veterinariasDelServer = await respuesta.json();
+      if (Array.isArray(veterinariasDelServer)) {
+        veterinarias = veterinariasDelServer;
+      }
+      if (veterinarias.length > 0) {
+        const htmlVeterinarias = veterinarias
+          .map(
+            (veterinaria, index) => `<tr>
+          <th scope="row">${index}</th>
+          <td>${veterinaria.documento}</td>
+          <td>${veterinaria.nombre}</td>
+          <td>${veterinaria.apellido}</td>
+          <td>
+              <div class="btn-group" role="group" aria-label="Basic example">
+                  <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
+                  <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
+              </div>
+          </td>
+        </tr>`
+          )
+          .join("");
+        listaVeterinarias.innerHTML = htmlVeterinarias;
+        Array.from(document.getElementsByClassName("editar")).forEach(
+          (botonEditar, index) => (botonEditar.onclick = editar(index))
+        );
+        Array.from(document.getElementsByClassName("eliminar")).forEach(
+          (botonEliminar, index) => (botonEliminar.onclick = eliminar(index))
+        );
+        return;
+      }
+      listaVeterinarias.innerHTML = `<tr>
+      <td colspan="5" class="lista-vacia">No hay veterinarias</td>
+    </tr>`;
+    } catch (error) {
+      console.log({ error });
+      $(".alert").show();
     }
-    listarVeterinarias();
-    resetModal();
+  }
+
+
+async function enviarDatos(evento){
+    evento.preventDefault();
+    try {
+        const datos = {
+            nombre: nombre.value,
+            apellido: apellido.value,
+            pais: pais.value,
+            documento: documento.value
+        };
+        const accion = btnGuardar.innerHTML
+        const urlEnvio = url;
+        let method = 'POST';
+        if(accion === 'Editar'){
+            //editar
+            method = 'PUT';
+            urlEnvio += `/${indice.value}`;
+        }
+        const respuesta = await fetch(urlEnvio, {
+            method,
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(datos),
+            mode: "cors",
+        });
+        if (respuesta.ok) {
+            listarMascotas();
+            resetModal();
+        }
+    } catch (error) {
+        console.log({ error });
+        $(".alert").show();
+    }
 }
 
 function editar(index){
@@ -72,7 +97,7 @@ function editar(index){
         nombre.value = veterinaria.nombre;
         apellido.value = veterinaria.apellido;
         pais.value = veterinaria.pais;
-        identificacion.value = veterinaria.identificacion;
+        documento.value = veterinaria.documento;
         indice.value = index;
     }
 }
@@ -81,7 +106,7 @@ function resetModal(){
     nombre.value = '';
     apellido.value = '';
     pais.value = '';
-    identificacion.value = '';
+    documento.value = '';
     indice.value = '';
     btnGuardar.innerHTML = 'Crear';
 }
